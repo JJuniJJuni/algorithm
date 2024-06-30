da724d192a6755653f89c2deda15104e37f41659
JJuniJJuni <qkrgud305@naver.com>
2024-07-01 01:48:22 +0900

    refactor: 전화번호 목록 lvl2 해싱으로 변경

--- programmers/lvl02/phone_book/main.py
+++ programmers/lvl02/phone_book/main.py
@@ -1,20 +1,13 @@
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] != target and arr[mid].startswith(target):
-            return 1
-        elif arr[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return -1
 
 
 def solution(phone_book):
     phone_book.sort()
+    numbers = set()
     for number in phone_book:
-        if binary_search(phone_book, number) > 0:
-            return False
+        temp = ""
+        for digit in number:
+            temp += digit
+            if temp in numbers and temp != number:
+                return False
+            numbers.add(number)
     return True
