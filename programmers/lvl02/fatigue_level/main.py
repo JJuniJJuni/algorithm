83f69a475ec5b16a41ccc808bbff3fe064dd5786
JJuniJJuni <qkrgud305@naver.com>
2024-07-10 02:39:39 +0900

    refactor: 피로도 lvl2 순열 라이브러리 참조

--- programmers/lvl02/fatigue_level/main.py
+++ programmers/lvl02/fatigue_level/main.py
@@ -1,7 +1,9 @@
+from itertools import permutations
+
+
 def solution(k, dungeons):
     limit = len(dungeons)
-    check = limit * [False]
-    answer = [-1]
+    answer = -1
 
     def get_cnt(curr, k):
         cnt = 0
@@ -14,15 +16,6 @@ def solution(k, dungeons):
                 break
         return cnt
 
-    def pen(idx, limit, k, curr):
-        if idx == limit:
-            cnt = get_cnt(curr, k)
-            answer[0] = max(answer[0], cnt)
-            return
-        for i in range(limit):
-            if not check[i]:
-                check[i] = True
-                pen(idx + 1, limit, k, curr + [i])
-                check[i] = False
-    pen(0, limit, k, [])
-    return answer[0]
+    for order in permutations(range(limit)):
+        answer = max(answer, get_cnt(order, k))
+    return answer
