7f2a0d0e377c9e7727f84ceb0868db02bc5feb15
JJuniJJuni <qkrgud305@naver.com>
2024-07-20 23:18:03 +0900

    feat: lvl2 큰 수 만들기 답변 참고. 이제까지의 데이터 stack 저장해서 현재 digit보다 작으면 계속 깎는게 핵심!

--- programmers/lvl02/make_big_number/main.py
+++ programmers/lvl02/make_big_number/main.py
@@ -1,21 +1,10 @@
-def largest_idx(curr_idx, limit, reversed_number):
-    result = -1
-    max_number = -1
-    for idx, number in enumerate(reversed_number[curr_idx:limit]):
-        if max_number <= ord(number):
-            result = idx
-            max_number = ord(number)
-    return curr_idx + result
-
-
 def solution(number, k):
-    reversed_number = number[::-1]
-    idxes = list(range(len(number) - k))
-    result = []
-    last_idx = len(number)
-    while idxes:
-        idx = idxes.pop()
-        next_idx = largest_idx(idx, last_idx, reversed_number)
-        result.append(reversed_number[next_idx])
-        last_idx = next_idx
-    return ''.join(result)
+    stack = []
+    for digit in number:
+        while k > 0 and stack and stack[-1] < digit:
+            k -= 1
+            stack.pop()
+        stack.append(digit)
+    if k > 0:
+        stack = stack[:-k]
+    return ''.join(stack)
--- programmers/lvl02/make_big_number/tests.py
+++ programmers/lvl02/make_big_number/tests.py
@@ -3,10 +3,6 @@ from unittest import TestCase
 from main import *
 
 class TestSolution(TestCase):
-    def test_large_number(self):
-        self.assertEqual(2, largest_idx(1, 4, [(2, "9"), (0, "4")]))
-        self.assertEqual(4, largest_idx(1, 7,[(0, "4"), (4, "3"), (1, "3")]))
-
     def test_solution(self):
         self.assertEqual("94", solution("1924", 2))
         self.assertEqual("3234", solution("1231234", 3))
